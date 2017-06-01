38e4c89c1a5d1003f4cf3e60b0ba8d650bec91e8
cos404 <22division7@gmail.com>
2017-06-01 22:48:43 +0300

    Add: using 'Environment Variables'

--- app.rb
+++ app.rb
@@ -2,22 +2,22 @@ require 'yaml'
 require 'telegram/bot'
 require './twitch-api/base.rb'
 
-token = '382897175:AAHTpTpyWAakEIwBpasJQzQ_gyew4tf9ERI'
+token = ENV['TOKEN']
 
 @twitch = Base.new({
-  client_id: '9hkyghtspdabk7g2eyqpqntayfmnzr',
-  secret_key: 'ry7mv0tjq2v2jbfsz0zhjccm8b21d0',
+  client_id: ENV['CLIENT_ID'],
+  secret_key: ENV['SECRET_KEY'],
   redirect_uri: 'https://localhost:3000/',
   scope: ["user_read", "channel_read", "user_follows_edit"],
-  access_token: '0f2wea68a2yhgzhhvkhpcg4gj4by1v'
+  access_token: ENV['ACCESS_TOKEN']
 })
-
 
 last_update = YAML.load_file('streamers.yml')
 
 Telegram::Bot::Client.run(token) do |bot|
   puts "Run"
 
+  # STREAM CHECKOUT
   Thread.new do
     loop do
       puts "Stream check: RUN"
@@ -26,22 +26,21 @@ Telegram::Bot::Client.run(token) do |bot|
 
         last_update[keys].length.times do |streamer_num|
 
-          s1 = last_update[keys][streamer_num][0]
-          s = @twitch.stream(s1)
+          streamer_name = last_update[keys][streamer_num][0]
+          result = @twitch.stream(streamer_name)
 
-          bot.api.sendMessage(chat_id: keys, text: "#{s1}: Stream RUN") if s["stream"] && !last_update[keys][streamer_num][1]
-          last_update[keys][streamer_num][1] = true if s["stream"]
-          last_update[keys][streamer_num][1] = false unless s["stream"]
+          bot.api.sendMessage(chat_id: keys, text: "#{streamer_name}: Stream RUN") if result["stream"] && !last_update[keys][streamer_num][1]
+          last_update[keys][streamer_num][1] = true if result["stream"]
+          last_update[keys][streamer_num][1] = false unless result["stream"]
         end
       end
       puts "Stream check: END"
-      sleep(300)
+      sleep 300
     end
   end
 
   # TELEGRAM COMMANDS
   bot.listen do |message|
-    puts "TELEGRAM BOT"
     msg = message.text
     chat_id = message.chat.id
     first_name = message.from.first_name
@@ -50,15 +49,12 @@ Telegram::Bot::Client.run(token) do |bot|
         bot.api.sendMessage(chat_id: chat_id, text: "Hello, #{first_name}")
       when /\/add (.+)/
         streamer = msg.sub(/\/add /, "")
-
         last_update = {} unless last_update
         last_update[chat_id] = [] if last_update[chat_id] == nil
         last_update[chat_id] << [streamer, false] unless last_update[chat_id].include?(streamer)
 
-        File.open('streamers.yml','w+') do |h|
-          puts h.to_yaml
-          puts h.to_s
-          h.write(last_update.to_yaml)
+        File.open('streamers.yml','w+') do |f|
+          f.write(last_update.to_yaml)
         end
     end
   end
