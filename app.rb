baca03670c2b16c21b09d11bf3bdd189ab35a193
cos404 <22division7@gmail.com>
2017-06-01 21:38:57 +0300

    Add: added thread in which the 'twitch stream' is checked

--- app.rb
+++ app.rb
@@ -15,46 +15,51 @@ token = '382897175:AAHTpTpyWAakEIwBpasJQzQ_gyew4tf9ERI'
 
 last_update = YAML.load_file('streamers.yml')
 
+Telegram::Bot::Client.run(token) do |bot|
+  puts "Run"
 
-# Telegram::Bot::Client.run(token) do |bot|
-#   puts "Run"
-#   bot.listen do |message|
-#     msg = message.text
-#     chat_id = message.chat.id
-#     first_name = message.from.first_name
-#     case msg
-#       when '/start'
-#         bot.api.sendMessage(chat_id: chat_id, text: "Hello, #{first_name}")
-#       when /\/add (.+)/
-#         streamer = msg.sub(/\/add /, "")
+  Thread.new do
+    loop do
+      puts "Stream check: RUN"
+      last_update.length.times do |group_num|
+        keys = last_update.keys[group_num]
 
-#         last_update = {} unless last_update
-#         last_update[chat_id] = [] if last_update[chat_id] == nil
-#         last_update[chat_id] << [streamer, false] unless last_update[chat_id].include?(streamer)
+        last_update[keys].length.times do |streamer_num|
 
-#         File.open('streamers.yml','w+') do |h|
-#           puts h.to_yaml
-#           puts h.to_s
-#           h.write(last_update.to_yaml)
-#         end
+          s1 = last_update[keys][streamer_num][0]
+          s = @twitch.stream(s1)
 
-#     end
-#   end
-# end
-
-loop do
-  last_update.length.times do |group_num|
-    last_update[last_update.keys[group_num]].length.times do |streamer_num|
-
-      keys = last_update.keys[group_num]
-      s = last_update[keys][streamer_num][0]
-      s = @twitch.stream(s)
-
-      puts "#{streamer_num}: Stream RUN" if s["stream"] && last_update[keys][streamer_num][1] == false
-      last_update[keys][streamer_num][1] = true if s["stream"]
-      last_update[keys][streamer_num][1] = false unless s["stream"]
+          bot.api.sendMessage(chat_id: keys, text: "#{s1}: Stream RUN") if s["stream"] && !last_update[keys][streamer_num][1]
+          last_update[keys][streamer_num][1] = true if s["stream"]
+          last_update[keys][streamer_num][1] = false unless s["stream"]
+        end
+      end
+      puts "Stream check: END"
+      sleep(300)
     end
   end
-  puts "Stream run: cancel"
-  sleep(6)
+
+  # TELEGRAM COMMANDS
+  bot.listen do |message|
+    puts "TELEGRAM BOT"
+    msg = message.text
+    chat_id = message.chat.id
+    first_name = message.from.first_name
+    case msg
+      when '/start'
+        bot.api.sendMessage(chat_id: chat_id, text: "Hello, #{first_name}")
+      when /\/add (.+)/
+        streamer = msg.sub(/\/add /, "")
+
+        last_update = {} unless last_update
+        last_update[chat_id] = [] if last_update[chat_id] == nil
+        last_update[chat_id] << [streamer, false] unless last_update[chat_id].include?(streamer)
+
+        File.open('streamers.yml','w+') do |h|
+          puts h.to_yaml
+          puts h.to_s
+          h.write(last_update.to_yaml)
+        end
+    end
+  end
 end
