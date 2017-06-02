123b5b756bf42317ab1f6a3add2c0f38da76be73
cos404 <22division7@gmail.com>
2017-06-02 22:21:12 +0300

    Add: in group chat can use only admins

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require 'telegram/bot'
 require './twitch-api/base.rb'
 
 token = ENV['TOKEN']
-
+check_timeout = ENV['CHECK_TIMEOUT'] || 10
 @twitch = Base.new(
   client_id: ENV['CLIENT_ID'],
   secret_key: ENV['SECRET_KEY'],
@@ -12,31 +12,37 @@ token = ENV['TOKEN']
   access_token: ENV['ACCESS_TOKEN']
 )
 
-streamers_list = YAML.load_file('streamers.yml')
+# streamers_list = YAML.load_file('streamers.yml')
+
+File.open('streamers.yml', 'w+') do |f|
+  puts @streamers_list = YAML.load_file(f)
+end
 
 Telegram::Bot::Client.run(token) do |bot|
   # STREAM CHECKOUT
   Thread.new do
     loop do
       puts 'Stream check: RUN'
-      streamers_list.length.times do |group_num|
-        keys = streamers_list.keys[group_num]
-        chat_id = streamers_list[keys]
+      if @streamers_list
+        @streamers_list.length.times do |group_num|
+          keys = @streamers_list.keys[group_num]
+          chat_id = @streamers_list[keys]
 
-        chat_id.length.times do |streamer_num|
-          streamer = chat_id[streamer_num]
-          result = @twitch.stream(streamer[0])
+          chat_id.length.times do |streamer_num|
+            streamer = chat_id[streamer_num]
+            result = @twitch.stream(streamer[0])
 
-          puts "#{Time.now.strftime("%H:%M")} Stream: #{streamer[0]}."
+            puts "#{Time.now.strftime('%H:%M')} Stream: #{streamer[0]}."
 
-          bot.api.sendMessage(chat_id: keys, text: "https://www.twitch.tv/#{streamer[0]}") if result['stream'] && !streamer[1]
+            bot.api.sendMessage(chat_id: keys, text: "https://www.twitch.tv/#{streamer[0]}") if result['stream'] && !streamer[1]
 
-          streamer[1] = true if result['stream']
-          streamer[1] = false unless result['stream']
+            streamer[1] = true if result['stream']
+            streamer[1] = false unless result['stream']
+          end
         end
       end
-      puts 'Stream check: END'
-      sleep 10
+        puts 'Stream check: END'
+        sleep check_timeout
     end
   end
 
@@ -44,18 +50,25 @@ Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     msg = message.text
     chat_id = message.chat.id
+    user_id = message.from.id
     first_name = message.from.first_name
     case msg
     when '/start'
       bot.api.sendMessage(chat_id: chat_id, text: "Hello, #{first_name}")
     when %r{\/add (.+)}
-      streamer = msg.sub(%r{\/add }, '').gsub(" ", "")
-      streamers_list = {} unless streamers_list
-      streamers_list[chat_id] = [] if streamers_list[chat_id].nil?
-      streamers_list[chat_id] << [streamer, false] unless streamers_list[chat_id].include?(streamer)
+      result = bot.api.getChatMember(chat_id: chat_id, user_id: user_id)
 
-      File.open('streamers.yml', 'w+') do |f|
-        f.write(streamers_list.to_yaml)
+      if !%w[administrator creator].include?(result['result']['status']) && chat_id < 0
+        bot.api.sendMessage(chat_id: chat_id, text: 'You not admin!')
+      else
+        streamer = msg.sub(%r{\/add }, '').delete(' ')
+        @streamers_list = {} unless @streamers_list
+        @streamers_list[chat_id] = [] if @streamers_list[chat_id].nil?
+        @streamers_list[chat_id] << [streamer, false] unless @streamers_list[chat_id].include?(streamer)
+
+        File.open('streamers.yml', 'w+') do |f|
+          f.write(@streamers_list.to_yaml)
+        end
       end
     end
   end
